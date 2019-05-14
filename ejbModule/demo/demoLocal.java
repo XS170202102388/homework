d8b250051dc34cf52067ebb887b7cb3c18f3d4be
XS170202102388 <1057806761@qq.com>
2019-05-14 08:51:17 +0800

    Signed-off-by: XS170202102388 <1057806761@qq.com>

---
+++ ejbModule/demo/demoLocal.java
@@ -0,0 +1,11 @@
+package demo;
+
+import javax.ejb.Local;
+
+/**
+ * demo local interface
+ */
+@Local
+public interface demoLocal {
+
+}
---
+++ ejbModule/demo/jiaowu.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class jiaowu {
+	public static void main(String[] args) {
+		System.out.println("good!");
+	}
+}
+
