242d94c522d6876ff8e9e92ed740d069e39bdb5d
XS170202102388 <1057806761@qq.com>
2019-05-14 08:51:17 +0800

    Signed-off-by: XS170202102388 <1057806761@qq.com>

---
+++ ejbModule/demo/demo.java
@@ -0,0 +1,17 @@
+package demo;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class demo
+ */
+@Stateless
+public class demo implements demoLocal {
+
+	/**
+     * Default constructor. 
+     */
+    public demo() {
+        
+    }
+}
---
+++ ejbModule/demo/student.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class student {
+	 public String name;
+	   public String address;
+	   student(){
+		   
+	   }
+
+
+}
